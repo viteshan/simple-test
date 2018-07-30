cb6854a880e89edc0af96ff682a3ba4584c4a7bb
viteshan <viteshan@163.com>
2018-07-30 18:23:34 +0800

    init repos

---
+++ disruptor_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/smartystreets/go-disruptor"
+	"testing"
+	"runtime"
+	"time"
+)
+
+const (
+	BufferSize   = 1024 * 64
+	BufferMask   = BufferSize - 1
+	Iterations   = 1000000 * 1000
+	Reservations = 16
+)
+
+var ring = [BufferSize]int64{}
+
+
+
+func TestDisruptor(t *testing.T) {
+	runtime.GOMAXPROCS(2)
+
+	controller := disruptor.
+		Configure(BufferSize).
+		WithConsumerGroup(SampleConsumer{}).
+		Build()
+
+	controller.Start()
+
+	started := time.Now()
+	publish(controller.Writer())
+	finished := time.Now()
+
+	controller.Stop()
+	fmt.Println(Iterations, finished.Sub(started))
+}
+
+func TestDisruptorEvent(t *testing.T){
+	disruptor.Configure(BufferSize)
+
+}
+
+
+func publish(writer *disruptor.Writer) {
+	sequence := disruptor.InitialSequenceValue
+	for sequence <= Iterations {
+		sequence = writer.Reserve(Reservations)
+		for lower := sequence - Reservations + 1; lower <= sequence; lower++ {
+			ring[lower&BufferMask] = lower
+		}
+
+		writer.Commit(sequence-Reservations+1, sequence)
+	}
+}
+
+// func publish(writer *disruptor.Writer) {
+// 	sequence := disruptor.InitialSequenceValue
+// 	for sequence <= Iterations {
+// 		sequence += Reservations // only an advantage at smaller reservations, e.g. 1-4?
+// 		writer.Await(sequence)
+// 		for lower := sequence - Reservations + 1; lower <= sequence; lower++ {
+// 			ring[lower&BufferMask] = lower
+// 		}
+// 		writer.Commit(sequence-Reservations+1, sequence)
+// 	}
+// }
+
+type SampleConsumer struct{}
+
+func (this SampleConsumer) Consume(lower, upper int64) {
+	for lower <= upper {
+		message := ring[lower&BufferMask]
+		if message != lower {
+			fmt.Println("Race condition", message, lower)
+			panic("Race condition")
+		}
+		lower++
+	}
+}
---
+++ eventbus_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asaskevich/EventBus"
+	"testing"
+)
+
+func calculator1(a int, b int) {
+	fmt.Printf("calculator1:\t%d\n", a+b)
+}
+func calculator2(a int, b int) {
+	fmt.Printf("calculator2:\t%d\n", a+b)
+}
+func TestEventBus(t *testing.T) {
+	bus := EventBus.New()
+	bus.Subscribe("main:calculator", calculator1)
+	bus.Subscribe("main:calculator", calculator2)
+	bus.Publish("main:calculator", 20, 40)
+	bus.Unsubscribe("main:calculator", calculator1)
+	bus.Unsubscribe("main:calculator", calculator2)
+}
